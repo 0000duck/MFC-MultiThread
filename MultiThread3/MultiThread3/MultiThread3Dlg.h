19eb786097a30985edd03833e7f79166beaed23e
YellowMushroom <hak19930624@qq.com>
2016-12-15 11:35:47 +0800

    不断创建线程，直到不能创建为止，目前能创建的线程数在1390条左右

---
+++ MultiThread3/MultiThread3/MultiThread3Dlg.h
@@ -0,0 +1,35 @@
+
+// MultiThread3Dlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMultiThread3Dlg 对话框
+class CMultiThread3Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMultiThread3Dlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_MULTITHREAD3_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedTest();
+	int m_nCount;
+};
